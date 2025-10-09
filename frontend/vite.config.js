972de42e68fd0106116d05c16c51026179ffbf87
Vaibhav Vats <vats88690@gmail.com>
2025-10-09 08:08:09 +0530

    v1.1 --deploy on vercel version 1

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,10 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173, // or whatever port you're using
-  }
+  base: process.env.VITE_BASE_URL || "//zuber-webste",
 })
 
 
